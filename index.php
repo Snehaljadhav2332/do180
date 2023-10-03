4b0c30495ae28a933f1a092543ca86bd7a990e6c
Snehal S. Jadhav <146528987+Snehaljadhav2332@users.noreply.github.com>
2023-10-03 18:54:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page </h1>
+<h1>My first PHP page  </h1>
 
 <?php
-echo "hello World from test FROM S2I 2nd time VERSION 4.2 "
+echo "hello World from Snehal S. Jadhav "
 ?>
 
 </body>
